e1d5eb7d2829147e1a75ba1836e6d3aac18dd4e5
Ross Patterson <me@rpatterson.net>
2012-09-23 10:52:45 -0700

    Move some stuff into set up

--- src/buster_selenium/case.py
+++ src/buster_selenium/case.py
@@ -28,23 +28,24 @@ class BusterJSTestCase(unittest.TestCase):
             # available
             self.layer.setUp()
 
-    def run(self, result=None):
         options = [option.strip() for option in """${options}""".split()
                    if option.strip()]
         output = TODO  # handle XML how?
         if output and not os.path.isdir(output):
             os.makedirs(output)
-                    stdout = sys.stdout
-                    if output:
-                        result = dirpath[len(p):].strip('/').replace(
-                            '/', '-') + '.xml'
-                        stdout = open(os.path.join(output, result), 'w')
-                    retcode = subprocess.call(
-                        [os.path.join('bin', 'buster-test')] + options
-                        + ['--config', os.path.join(dirpath, filename)]
-                        + sys.argv[1:], stdout=stdout)
-                    if retcode:
-                        failures.append(dirpath)
+        stdout = sys.stdout
+        if output:
+            result = dirpath[len(p):].strip('/').replace(
+                '/', '-') + '.xml'
+            stdout = open(os.path.join(output, result), 'w')
+        self.stdout = stdout
+
+    def run(self, result=None):
+        retcode = subprocess.call(
+            [self.bin] + options + ['--config', os.path.join(dirpath, filename)]
+            + sys.argv[1:], stdout=stdout)
+        if retcode:
+            failures.append(dirpath)
 
     def tearDown(self):
         if testrunner is None:
